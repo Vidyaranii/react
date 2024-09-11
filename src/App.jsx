30eadf52f1ed0bedc38594dc673503577c9a659b
vidyaranii <vidhyaranikomma@gmail.com>
2024-09-11 15:56:51 +0530

    v

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-import Table from '../table/task1'
 import Apple from '../table/task2'
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
   
   return (
     <>
-  <Apple/>
+<Apple/>
  </>
   )
 }
