d2a86b860cb237712410e8f5bc5561f9c88f6476
vidyaranii <vidhyaranikomma@gmail.com>
2024-09-12 11:59:17 +0530

    ss

---
+++ src/card/card.jsx
@@ -0,0 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import { Card } from 'react-bootstrap';
+// import CustomToast from '../toast/toast';
+import InstagramHeart from './animated';
+import InifiniteLoader from '../spinner/spinner';
+import CustomDNALoader from '../loarder/loader';
+
+function Basic({title,vidya, removeHandler,ind}) {
+  return (
+    <Card>
+     
+      <Card.Body>
+     
+        <Card.Title> {title} </Card.Title>
+       
+   
+        <CustomDNALoader/>
+        <InifiniteLoader color={'red'}/>
+        {/* <CustomToast message={'i am card'}/> */}
+        <InstagramHeart/>
+       
+        <Button variant="primary" onClick={() =>removeHandler(ind)}>Remove</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Basic;
---
+++ src/loarder/loader.jsx
@@ -0,0 +1,15 @@
+import {DNA} from "react-loader-spinner"
+const CustomDNALoader =()=>{
+    return(
+       
+        <DNA
+            visible={true}
+            height="120"
+            width="120"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+            />)
+   
+}
+export default CustomDNALoader
