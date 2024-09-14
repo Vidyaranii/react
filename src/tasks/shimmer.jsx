7b578c77c63751be48a43fb1fe3311e8211e0405
vidyaranii <vidhyaranikomma@gmail.com>
2024-09-14 17:02:46 +0530

    c

---
+++ src/tasks/shimmer.jsx
@@ -0,0 +1,9 @@
+// import React, { Component } from "react";
+// import { ShimmerButton } from "react-shimmer-effects";
+
+// class Example extends Component {
+//   render() {
+//     return <ShimmerButton size="md" />;
+//   }
+// }
+// export default Example
