ed9ec7601e46073b439f661e38e2d712b22799e2
vidyaranii <vidhyaranikomma@gmail.com>
2024-09-17 19:05:42 +0530

    commit

--- src/loarder/loader.jsx
+++ src/loarder/loader.jsx
@@ -1,15 +1,18 @@
 import {DNA} from "react-loader-spinner"
 const CustomDNALoader =()=>{
     return(
-       
+       <>
+       <h1>hello</h1>
         <DNA
+        
             visible={true}
             height="120"
             width="120"
             ariaLabel="dna-loading"
             wrapperStyle={{}}
             wrapperClass="dna-wrapper"
-            />)
-   
+            />
+            </>)
+          
 }
 export default CustomDNALoader
